cc3949f81a0be679c998f0278ff5e07afe6211ae
Jon Morris-Pocock <jonmorrispocock@gmail.com>
2014-03-19 16:57:40 -0400

    Fixed model bug in last commit.

--- htdocs/application/config/autoload.php
+++ htdocs/application/config/autoload.php
@@ -112,7 +112,7 @@ $autoload['language'] = array();
 */
 
 //$autoload['model'] = array('MCats','MProducts','MOrders','MAdmins','MSizes','MColors', 'MPages', 'MSubscribers');
-$autoload['model'] = array('Product', 'Customer', 'Order', 'OrderItem');
+$autoload['model'] = array('Product', 'Customer', 'Order');
 
 
 /* End of file autoload.php */
