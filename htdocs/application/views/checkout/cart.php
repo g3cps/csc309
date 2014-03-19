e9e4ce38ce241f2460013ebbfd269a8bb9a9a7f9
Cheung Brian Pak Shing <g3cps@cdf.toronto.edu>
2014-03-19 03:02:38 -0400

    Show total amount of 50103$ in cart

--- htdocs/application/views/checkout/cart.php
+++ htdocs/application/views/checkout/cart.php
@@ -12,19 +12,22 @@
 	if ($this->session->userdata('logged_in') && $this->session->userdata('cart') && sizeof($this->session->userdata('cart')) > 0){
 		echo "<p>You can change quantity for each item here.</p>";
 		echo "<p>Set the quantity to 0 to remove item from cart</p>";
+		$total = 0;
 		foreach ($products as $product){
 			foreach ($this->session->userdata('cart') as $added){
 				if ($product->id == $added[0]){
 					echo "<img id='incart' src='" . base_url() . 
 						"images/product/" . $product->photo_url . 
 						"'></img>";
-					echo form_label($product->name);
+					echo form_label($product->name . " - $" . $product->price . " each");
 					echo form_input($added[0],$added[1]);
+					$total += $added[1] * $product->price;
 				}
 			}
 		}
 		echo form_submit('submit', 'Update');
 		echo form_close();
+		echo "<p>Total: $$total </p>";
 	} else { //Nothing is in the cart
 		echo "<p>Nothing in the cart :(</p>";
 	}
